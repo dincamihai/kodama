ee76ff15e14473facc1eb785a7ea4e5c89a7d8ba
Mihai Dincă <dincamihai@gmail.com>
2020-09-03 09:02:57 +0200

    Store to DB

--- kodama/consumer.py
+++ kodama/consumer.py
@@ -1,5 +1,7 @@
 import json
+import psycopg2
 from kafka import KafkaConsumer
+import logging
 
 
 def value_deserializer(value):
@@ -12,17 +14,41 @@ def get_consumer():
         bootstrap_servers=['localhost:9092'],
         auto_offset_reset='earliest',
         enable_auto_commit=True,
-        group_id='my-group',
+        group_id='kodama',
         value_deserializer=value_deserializer
     )
 
 
-def consume(consumer):
+def get_db_connection():
+    try:
+        conn=psycopg2.connect("host='localhost' dbname='kodama' user='kodama' password='kodama'")
+        return conn
+    except:
+        logging.error("Unable to connect to DB.")
+
+
+def insert(cur, data):
+    cur.execute(f"INSERT INTO checklog VALUES ('{data['url']}', {data['response_time']}, {data['return_code']}, {data['regex_matches']})")
+
+
+def store(conn, data, commit=False):
+    try:
+        insert(conn.cursor(), data)
+    except:
+        logging.error("Unable to write to DB.")
+    else:
+        if commit:
+            conn.commit()
+        else:
+            logging.warning("Not commiting to DB.")
+
+
+def consume(consumer, db):
     for message in consumer:
         message = message.value
-        print(message)
+        store(db, message, commit=True)
 
 
 if __name__ == '__main__':
     consumer = get_consumer()
-    consume(consumer)
+    consume(consumer, get_db_connection())
--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -1,5 +1,19 @@
 import kodama
-from mock import patch
+import psycopg2
+
+import pytest
+from mock import patch, Mock
+
+
+@pytest.fixture
+def db():
+    conn=psycopg2.connect("host='localhost' dbname='kodama' user='kodama' password='kodama'")
+    return conn
+
+
+@pytest.fixture
+def cur(db):
+    return db.cursor()
 
 
 @patch('kodama.consumer.KafkaConsumer')
@@ -11,6 +25,19 @@ def test_get_consumer(mock_KafkaConsumer):
         bootstrap_servers=['localhost:9092'],
         auto_offset_reset='earliest',
         enable_auto_commit=True,
-        group_id='my-group',
+        group_id='kodama',
         value_deserializer=kodama.consumer.value_deserializer
     )
+
+
+def test_store(db, cur):
+    data = {
+        'response_time': 10,
+        'url': 'http://yahoo.com',
+        'return_code': 200,
+        'regex_matches': True
+    }
+    kodama.consumer.store(db, data)
+    cur.execute("SELECT COUNT(*) FROM checklog;")
+    (result,) = cur.fetchone()
+    assert result == 1
