fcca1ea1b013cfb15c929309db43f3de16a007d7
Mihai Dincă <dincamihai@gmail.com>
2020-09-03 19:19:43 +0200

    Add row id and creation time

--- docker-entrypoint-initdb.d/01-create-tables.sh
+++ docker-entrypoint-initdb.d/01-create-tables.sh
@@ -3,9 +3,11 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE TABLE checklog (
-    url text,
-    response_time int,
-    return_code int,
-    regex_matches boolean
+    id INT GENERATED ALWAYS AS IDENTITY,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    url VARCHAR NOT NULL,
+    response_time INT NOT NULL,
+    return_code INT NOT NULL,
+    regex_matches BOOLEAN
 )
 EOSQL
